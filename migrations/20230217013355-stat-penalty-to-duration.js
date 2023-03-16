d8c25c745a2baad3fb85bfb1fd0405d849af1ca3
Jeremy Price <jrp114@yahoo.com>
2023-03-16 09:31:18 -0400

    fixed duration migration

--- migrations/20230217013355-stat-penalty-to-duration.js
+++ migrations/20230217013355-stat-penalty-to-duration.js
@@ -2,11 +2,13 @@ module.exports = {
   async up(db, client) {
     const penalties = await db.collection('stats').find({ penalty: { $exists: true } }).toArray()
     penalties.forEach(penalty => {
-      penalty.duration = (penalty.penalty.duration.minutes * 60) + penalty.penalty.duration.seconds
-      db.collection('stats').updateOne(
-        { _id: penalty._id },
-        { $set: { duration: penalty.duration } }
-      )
+      if (penalty.duration) {
+        penalty.duration = (penalty.penalty.duration.minutes * 60) + penalty.penalty.duration.seconds
+        db.collection('stats').updateOne(
+          { _id: penalty._id },
+          { $set: { duration: penalty.duration } }
+        )
+      }
     })
   },
 
